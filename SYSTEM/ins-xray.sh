ab7f8eb06b64c6f1fb274b486c9baafb0ddac8c8
Mylo <Yuhg1111@gmail.com>
2026-03-13 20:49:54 +0800

    Update ins-xray.sh

--- SYSTEM/ins-xray.sh
+++ SYSTEM/ins-xray.sh
@@ -143,7 +143,7 @@ cat > /etc/xray/config.json << END
        "streamSettings":{
          "network": "ws",
             "wsSettings": {
-                "path": "/vlessws/"
+                "path": "/vlessws/ "
           }
         }
      },
